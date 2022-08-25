8ca4a367e7db3c481d3f914be59fd87b5d773dba
Brandon Curry <91096420+BrandonCurry1225@users.noreply.github.com>
2022-08-24 21:05:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import matplotlib.pyplot as plt
 # FOR IMAGE DEMONSTRATION
 image = cv.imread('laneimage.jpg')
 
+# TO SHOW NORMAL IMAGE
 lane_image = np.copy(image)
 cv.imshow('road normal', lane_image)
 cv.waitKey(0)
 
+# CONVERT IMAGE TO GRAYSCALE
 gray = cv.cvtColor(lane_image, cv.COLOR_BGR2GRAY)
 cv.imshow('road grayscale', gray)
 cv.waitKey(0)
 
+# SMOOTHEN OUT GRAY IMAGE AND HIGHLIGHT EDGES
 smoothed_image = cv.GaussianBlur(gray, (5,5), 0)
 canny_image = cv.Canny(smoothed_image, 50, 150)
 cv.imshow('road with edges', canny_image)
 cv.waitKey(0)
 
+# FUNCTION TO CREATE MASK FOR REGION OF INTEREST
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[
@@ -30,8 +34,10 @@ def region_of_interest(image):
 
 cropped_image = region_of_interest(canny_image)
 
+# HOUGH TRANSFORM TO DETECT LINES
 lines = cv.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=50)
 
+# FUNCTION DISPLAYS LINES ON MASK
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
@@ -40,16 +46,17 @@ def display_lines(image, lines):
             cv.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 10)
     return line_image
 
-# show lines
+# DISPLAYS DETECTED LINES ON MASK
 line_image = display_lines(lane_image, lines)
 cv.imshow('lines of detection', line_image)
 cv.waitKey(0)
 
+# OVERLAPS LINES WITH LANE IMAGE
 combo_image = cv.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv.imshow('road with lines', combo_image)
 cv.waitKey(0)
 
-# FOR VIDEO
+# FOR VIDEO, LINES ARE DETECTED IN EACH FRAME
 cap = cv.VideoCapture('LaneDetectionTestVideo.mp4')
 while (cap.isOpened()):
 
